99d35b2d0a19c3459400b5717880db2da28fa4e4
wallacesintra <wallvxwallace962@gmail.com>
2024-03-06 12:10:32 +0300

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
 
 import datetime
+import uuid
+
 
 class BaseModel:
     """
-    Provides common attributes and methods for all models in the AirBnB clone application.
+    attributes and methods for models in the AirBnB clone application.
 
     Attributes:
         id (str): A unique identifier for each instance of the model.
-        created_at (datetime): The timestamp for when the instance was created.
-        updated_at (datetime): The timestamp for when the instance was last updated.
+        created_at (datetime): time created.
+        updated_at (datetime): time last updated.
     """
 
     def __init__(self, *args, **kwargs):
-        """Initializes the BaseModel instance with a unique ID and timestamps for creation and update."""
+        """
+        Initializes the BaseModel instance with a
+        unique ID and timestamps for creation and update.
+        """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = datetime.datetime.now()
@@ -21,18 +26,25 @@ class BaseModel:
 
     def save(self):
         """A method to save the model instance."""
-        # Implementation of save logic here.
-        # This could involve saving to a database or a file.
-        pass
+        self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
-        """Returns a dictionary representation of the model."""
-        return {
-            'id': self.id,
-            'created_at': self.created_at.isoformat() if self.created_at else None,
-            'updated_at': self.updated_at.isoformat() if self.updated_at else None
-        }
+        """
+        Returns a dictionary representation of the model.
+        """
+        model_dict = self.__dict__
+
+        for key in model_dict:
+            if key in ["created_at", "updated_at"]:
+                model_dict[key] = model_dict[key].isoformat()
+
+        model_dict["__class__"] = self.__class__.__name__
+        return model_dict
 
     def __str__(self):
-        """Returns a string representation of the model."""
-        return "[{}] ({})".format(self.__class__.__name__, self.id)
+        """
+        Returns a string representation of the model.
+        """
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__
+            )
